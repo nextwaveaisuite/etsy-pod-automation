b6c305a5607933829f8316ca2548d33ab423a31f
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-10-23 09:43:28 +1000

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
-    serverActions: { allowedOrigins: ['*'] }
-  },
-  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] }
+    appDir: true
+  }
 };
 export default nextConfig;
