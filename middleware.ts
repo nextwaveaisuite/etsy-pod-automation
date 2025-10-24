9a70c316ec0f5f19bc493734cb494eb732adabdd
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-10-24 16:56:48 +1000

    middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  // Public routes that don't require authentication
-  const publicRoutes = [
-    '/auth/login',
-    '/auth/signup',
-    '/auth/request-invite',
-    '/api/auth',
-  ];
-
-  // Check if the route is public
-  const isPublicRoute = publicRoutes.some(route => pathname.startsWith(route));
-
-  if (isPublicRoute) {
-    return NextResponse.next();
-  }
-
-  // For now, allow all routes (authentication disabled temporarily)
-  // TODO: Enable authentication after fixing auth-helpers import
+export function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|public).*)',
-  ],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|public).*)'],
 };
