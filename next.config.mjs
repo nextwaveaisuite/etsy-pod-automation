1177a63bec122db1f03aade3921d0ec4abf3b989
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-10-24 16:27:15 +1000

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: { allowedOrigins: ['*'] }
-  },
-  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] }
-};
-export default nextConfig;
